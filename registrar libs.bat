4c2ff33d903beeb859990b1e2f40462a776c12c9
Lugolu <lugolu@gmail.com>
2020-08-08 02:33:10 -0300

    remove libs

--- registrar libs.bat
+++ registrar libs.bat
@@ -1,5 +1,6 @@
 @echo off
 
+call mvn install:install-file -Dfile=babylon-theme-3.0.1.jar -DgroupId=org.primefaces -DartifactId=babylon-theme -Dversion=3.0.1 -Dpackaging=jar
+call mvn install:install-file -Dfile=jsflot-0.7.0.jar -DgroupId=jsflot -DartifactId=jsflot -Dversion=0.7.0 -Dpackaging=jar
 call mvn install:install-file -Dfile=ojdbc7.jar -DgroupId=com.oracle -DartifactId=ojdbc7 -Dversion=12.1.0.1 -Dpackaging=jar
 call mvn install:install-file -Dfile=RXTXcomm.jar -DgroupId=org.rxtx -DartifactId=rxtxcomm -Dversion=0.0.1 -Dpackaging=jar
-call mvn install:install-file -Dfile=babylon-theme-3.0.1.jar -DgroupId=org.primefaces -DartifactId=babylon-theme -Dversion=3.0.1 -Dpackaging=jar
