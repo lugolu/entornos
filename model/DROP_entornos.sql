520876143d432f533294483fad904b8818fa9fe1
Lugolu <lugolu@gmail.com>
2020-08-09 12:26:36 -0300

    model

--- model/DROP_entornos.sql
+++ model/DROP_entornos.sql
@@ -5,7 +5,7 @@
 /* Project name:                                                          */
 /* Author:                                                                */
 /* Script type:           Database drop script                            */
-/* Created on:            2020-08-09 02:59                                */
+/* Created on:            2020-08-09 12:25                                */
 /* ---------------------------------------------------------------------- */
 
 
@@ -13,17 +13,31 @@
 /* Drop foreign key constraints                                           */
 /* ---------------------------------------------------------------------- */
 
-ALTER TABLE ENTORNOS.ENTORNO_CLIENTE DROP CONSTRAINT FK_1_%table%;
+ALTER TABLE ENTORNOS.ENTORNO_CLIENTE DROP CONSTRAINT R_1_ENTORNO_CLIENTE;
 
-ALTER TABLE ENTORNOS.SERVIDOR DROP CONSTRAINT FK_1_%table%;
+ALTER TABLE ENTORNOS.SERVIDOR DROP CONSTRAINT R_1_SERVIDOR;
 
-ALTER TABLE eNTORNOS.CONTENEDOR DROP CONSTRAINT FK_1_%table%;
+ALTER TABLE eNTORNOS.CONTENEDOR DROP CONSTRAINT R_1_CONTENEDOR;
 
-ALTER TABLE ENTORNOS.APLICACION_CLIENTE DROP CONSTRAINT FK_1_%table%;
+ALTER TABLE ENTORNOS.APLICACION_CLIENTE DROP CONSTRAINT R_1_APLICACION_CLIENTE;
 
-ALTER TABLE ENTORNOS.APLICACION_CLIENTE DROP CONSTRAINT FK_2_%table%;
+ALTER TABLE ENTORNOS.APLICACION_CLIENTE DROP CONSTRAINT R_2_APLICACION_CLIENTE;
 
-ALTER TABLE ENTORNOS.APLICACION_CLIENTE DROP CONSTRAINT FK_3_%table%;
+ALTER TABLE ENTORNOS.APLICACION_CLIENTE DROP CONSTRAINT R_3_APLICACION_CLIENTE;
+
+ALTER TABLE ENTORNOS.DEPENDENCIA_APLICACION DROP CONSTRAINT R_1_DEPENDENCIA_APLICACION;
+
+ALTER TABLE ENTORNOS.DEPENDENCIA_APLICACION DROP CONSTRAINT R_2_DEPENDENCIA_APLICACION;
+
+/* ---------------------------------------------------------------------- */
+/* Drop table "ENTORNOS.DEPENDENCIA_APLICACION"                           */
+/* ---------------------------------------------------------------------- */
+
+/* Drop constraints */
+
+ALTER TABLE ENTORNOS.DEPENDENCIA_APLICACION DROP CONSTRAINT PK_DEPENDENCIA_APLICACION;
+
+DROP TABLE ENTORNOS.DEPENDENCIA_APLICACION;
 
 /* ---------------------------------------------------------------------- */
 /* Drop table "ENTORNOS.APLICACION_CLIENTE"                               */
