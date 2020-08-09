45b9766fd84cad190d3111331863f5cfc31dd79b
Lugolu <lugolu@gmail.com>
2020-08-09 02:36:33 -0300

    alter db

--- entornos.sql
+++ entornos.sql
@@ -1,14 +1,14 @@
 
 CREATE TABLE entornos.cliente (
-  id_cliente numeric NOT NULL,
+  id_cliente integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
   cliente character varying(40) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT cliente_pkey PRIMARY KEY (id_cliente)
 ) ;
 
 
 CREATE TABLE entornos.entorno_cliente (
-  id_entorno numeric NOT NULL,
-  id_cliente numeric NOT NULL,
+  id_entorno integer NOT NULL,
+  id_cliente integer NOT NULL,
   entorno character varying(40) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT entorno_cliente_pkey PRIMARY KEY (id_entorno, id_cliente),
     CONSTRAINT "FK_1_ENTORNO_CLIENTE" FOREIGN KEY (id_cliente)
